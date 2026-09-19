1165650fcb8ed73eec6fcbb47ac4f88366b0c6c4
las-r <128936184+las-r@users.noreply.github.com>
2026-09-19 13:58:08 -0400

    Make pattern and class

--- micro/lexer.py
+++ micro/lexer.py
@@ -2,3 +2,31 @@ import re
 
 # micro lexer
 # by las-r
+
+# regex pattern
+REGEX = re.compile(r"""
+    (?P<COMMENT>  //[^\n]*) |
+    (?P<NUMBER>   \d+(?:\.\d+)?) |
+    (?P<STRING>   "(?:\\.|[^"\\])*") |
+    (?P<KEYWORD>  \b(if|end|while|func|break|return)\b) |
+    (?P<OPER>     ==|!=|<=|>=|[+\-*/<>~&^():=,]) |
+    (?P<IDENT>    [a-zA-Z_]\w*) |
+    (?P<SKIP>     [ \t\r\n]+) |
+    (?P<MISMATCH> .)
+""", re.VERBOSE)
+
+# tokens class
+class Tokens:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.i = 0
+        
+    def can_eat(self, inc=1):
+        return self.i < len(self.tokens) - inc
+        
+    def eat(self, inc=1):
+        self.i += inc
+        return self.tokens[self.i - inc]
+    
+    def peek(self, off=0):
+        return self.tokens[self.i + off]
