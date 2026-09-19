0ea59ad24f073fbd3711022fed28a1a22b84a8dc
las-r <128936184+las-r@users.noreply.github.com>
2026-09-19 14:13:47 -0400

    Make basic nodes

--- micro/nodes.py
+++ micro/nodes.py
@@ -1,2 +1,28 @@
 # micro nodes
 # by las-r
+
+# data nodes
+class LiteralNode:
+    def __init__(self, value):
+        self.value = value
+    
+    def eval(self, env):
+        return self.value
+    
+# operation nodes
+class UnaryOpNode:
+    def __init__(self, op, a):
+        self.op = op
+        self.a = a
+    
+    def eval(self, env):
+        pass
+    
+class BinaryOpNode:
+    def __init__(self, a, op, b):
+        self.a = a
+        self.op = op
+        self.b = b
+    
+    def eval(self, env):
+        pass
